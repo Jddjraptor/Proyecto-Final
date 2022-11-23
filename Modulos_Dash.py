36cb877eb41b96f8e3543baba73b3191dcdfe278
chechogarcia <65240206+chechogarcia@users.noreply.github.com>
2022-11-22 23:19:27 -0500

    Update Modulos_Dash.py

--- Modulos_Dash.py
+++ Modulos_Dash.py
@@ -10,8 +10,7 @@ try:
     )
     print("Conexión exitosa")
     cursor=connection.cursor()
-    cursor.execute("select distinct u.Ciudad, sum(e.
-    Precio_Total), rank() over(order by e.Precio_Total) from Ubicacion as u, Envio as e, Cliente as c, Residencia as r where u.Ciudad = r.Ciudad and r.ID_Cliente = c.ID, e.ID_cliente = c.ID group by u.Ciudad")
+    cursor.execute("select distinct u.Ciudad, sum(e.Precio_Total), rank() over(partition by u.ciudad order by e.Precio_Total desc) from Ubicacion as u, Envio as e, Cliente as c, Residencia as r where u.Ciudad = r.Ciudad and r.ID_Cliente = c.ID, e.ID_cliente = c.ID order by u.Ciudad")
     #Consulta2
     #cursor.execute("select P.Nombre, rank() over(order by sum(Precio_Total))")
     rows=cursor.fetchall()
